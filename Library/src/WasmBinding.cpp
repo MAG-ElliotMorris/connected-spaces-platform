1293fb9a429448f9d05b8516e085889bcaa72b32
Elliot Morris <Elliot.Morris@magnopus.com>
2025-10-09 15:57:02 +0100

    Async with callback struct
    # TICKETID - Replace with the Jira Ticket ID
    # CATEGORY - Replace with one of the following:
    # -> fix - A bug fix.
    # -> feat - Introduces a new feature to the codebase.
    # -> build - A change to the project's build system.
    # -> chore - Commit includes necessary technical product or repository tasks.
    # -> ci - Changes to continuous integration (CI) configuration or scripts.
    # -> docs - Everything related to documentation.
    # -> style - Indicates changes made solely to the code formatting or style.
    # -> refac - Changes to formatting and whitespace, no code changes.
    # -> perf - Changes to improve code performance.
    # -> test - Adding, modifying, or improving project test cases.
    #
    # Optional Breaking Change Indicator - append ! to the category,
    # e.g. feat!: title
    #
    # TITLE - Include a brief description.
    #
    # DESCRIPTION
    # Include a bullet pointed list of what is included in the commit, what
    # do these changes add? Why has it been done this way?
    # Remember to leave at least one blank line between the title and the body.
    #

--- Library/src/WasmBinding.cpp
+++ Library/src/WasmBinding.cpp
@@ -29,18 +29,35 @@ class EntityCreatedCallbackJSShim : public csp::multiplayer::EntityCreatedCallba
 {
 public:
     EntityCreatedCallbackJSShim(EntityCreatedCb jsCB)
-        : m_jsCallback(jsCB){}
+        : m_jsCallback(jsCB)
+    {
+    }
     void Call(csp::multiplayer::SpaceEntity* e) override { m_jsCallback(emscripten::val(e)); }
 
 private:
     EntityCreatedCb m_jsCallback;
 };
 
+class EntityCreatedPromiseJSShim : public csp::multiplayer::EntityCreatedCallback
+{
+public:
+    EntityCreatedPromiseJSShim(emscripten::val Resolve, emscripten::val Reject)
+        : m_Resolve(Resolve)
+        , m_Reject(Reject)
+    {
+    }
+    void Call(csp::multiplayer::SpaceEntity* e) override { m_Resolve(emscripten::val(e)); }
+
+private:
+    emscripten::val m_Resolve;
+    emscripten::val m_Reject;
+};
+
 // This is just to prove that we can keep the callbacks alive. You could use shared_ptrs instead but it'd be a little more intricate.
 // Obviously cleaning these up will be a problem, honestly just cleaning up one-shot CBs on point of call, and having a "deregisterCB"
 // for long-lived ones is probably the best. Simple, cavemany. No-one really cares to deregister long lived callbacks anyhow.
-static std::unordered_map<uint64_t, EntityCreatedCallbackJSShim>
-    Callbacks;
+static std::unordered_map<uint64_t, EntityCreatedCallbackJSShim> Callbacks;
+static std::unordered_map<uint64_t, EntityCreatedPromiseJSShim> AsyncCallbacks; // Wonder if these could be the same buffer?
 
 // You're probably wondering why we don't just bind directly to the member function?
 // This is a tsgen limitation, as it currently cant understand std::function, maybe it will/does in a future emscripten version?
@@ -51,42 +68,11 @@ static void IRealtimeEngine_createEntity_js(csp::common::IRealtimeEngine& Self, 
 {
     static uint64_t CBId = 0;
     CBId++;
-
-    Callbacks.emplace(CBId, EntityCreatedCallbackJSShim{jsEntityCreatedCallback});
+    Callbacks.emplace(CBId, EntityCreatedCallbackJSShim { jsEntityCreatedCallback });
 
     auto& cb = Callbacks.at(CBId);
     Self.CreateEntity(Name, SpaceTransform, Parent, cb);
 }
-
-// Use the emscripten api to essentially implement a 1:1 JS promise object
-/*
-static SpaceEntityPromise IRealtimeEngine_createEntity_js_async(csp::common::IRealtimeEngine& Self,
-                        const std::string& Name,
-                        const csp::multiplayer::SpaceTransform& SpaceTransform,
-                        const std::optional<uint64_t>& Parent)
-{
-    // Resolve and Reject are how promises are implemented in JS
-    auto executor = std::function<void(emscripten::val resolve, emscripten::val reject)>(
-        [&](emscripten::val resolve, emscripten::val reject) {
-            try {
-                Self.CreateEntity(Name, SpaceTransform, Parent,
-                    // Success path, is either null or not null ... shocker!
-                    [resolve = std::move(resolve)](csp::multiplayer::SpaceEntity* e) {
-                        resolve(e ? emscripten::val(e) : emscripten::val::null());
-                    }
-                );
-            // Errors, reject promise
-            } catch (const std::exception& ex) {
-                reject(emscripten::val(ex.what()));
-            } catch (...) {
-                reject(emscripten::val("CreateEntity threw an unknown C++ exception"));
-            }
-        });
-
-    // Return a javascript promise object wrapping our promise implementation. Neat!
-    return SpaceEntityPromise(emscripten::val::global("Promise").new_(val(executor)));
-}
-*/
 
 // This is JS, don't get scared. We just need handles to the promise control knobs.
 EM_JS(EM_VAL, MakeJSPromise, (), {
@@ -99,6 +85,27 @@ EM_JS(EM_VAL, MakeJSPromise, (), {
     // you should immediately take C++ ownership of it as a ::val with val::take_ownership())
     return Emval.toHandle({ p : p, res : res, rej : rej });
 });
+
+static SpaceEntityPromise IRealtimeEngine_createEntity_js_async(csp::common::IRealtimeEngine& Self, const std::string& Name,
+    const csp::multiplayer::SpaceTransform& SpaceTransform, const std::optional<uint64_t>& Parent)
+{
+
+    // Direcly call JS to get a { p, res, rej } structure without needing a (potentially impossible) JS->C++ conversion.
+    emscripten::val PromisePack = val::take_ownership(MakeJSPromise());
+    emscripten::val Promise = PromisePack["p"];
+    emscripten::val Resolve = PromisePack["res"];
+    emscripten::val Reject = PromisePack["rej"];
+
+    static uint64_t CBId = 0;
+    CBId++;
+    AsyncCallbacks.emplace(CBId, EntityCreatedPromiseJSShim { Resolve, Reject });
+    auto& cb = Callbacks.at(CBId);
+
+    Self.CreateEntity(Name, SpaceTransform, Parent, cb);
+
+    return SpaceEntityPromise(Promise);
+}
+
 /*
 static SpaceEntityPromise IRealtimeEngine_createEntity_js_async(csp::common::IRealtimeEngine& Self, const std::string& Name,
     const csp::multiplayer::SpaceTransform& SpaceTransform, const std::optional<uint64_t>& Parent)
@@ -155,8 +162,8 @@ EMSCRIPTEN_BINDINGS(csp_emscripten_bindings)
     // Argument names in TS are defined here by the param list (undocumented feature!
     // https://github.com/emscripten-core/emscripten/issues/22001#issuecomment-2133940668)
     class_<csp::common::IRealtimeEngine>("IRealtimeEngine")
-        .function("createEntity(name, transform, parent, entityCreatedCallback)", &IRealtimeEngine_createEntity_js, allow_raw_pointers());
-     //   .function("createEntityAsync(name, transform, parent)", &IRealtimeEngine_createEntity_js_async, allow_raw_pointers());
+        .function("createEntity(name, transform, parent, entityCreatedCallback)", &IRealtimeEngine_createEntity_js, allow_raw_pointers())
+        .function("createEntityAsync(name, transform, parent)", &IRealtimeEngine_createEntity_js_async, allow_raw_pointers());
 
     // Subclass
     class_<csp::multiplayer::OfflineRealtimeEngine, base<csp::common::IRealtimeEngine>>("OfflineRealtimeEngine")
